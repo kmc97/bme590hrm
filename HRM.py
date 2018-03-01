703ebba5faf466724b39f954d9d1503bd8acc7cb
kmc97 <kmc97@duke.edu>
2018-03-01 10:12:05 -0500

    updated file to reflect less gross class usage

--- HRM.py
+++ HRM.py
@@ -4,38 +4,66 @@ from scipy.signal import savgol_filter, argrelmax
 import math
 
 
-class ImportData:
+class ReadWriteData:
 
     def __init__ (self, filename = None):
         self.filename = filename
-
+        self.file_data = None
+        self.data = None
+     
     def read_in_data(self):
         """Function takes in a csv file name and returns a numpy array
 
         :param self.filename: csv filename
-        :returns file_data_to_numpy: a 2d numpy array with time and voltage
+        :returns self.file_data: a 2d numpy array with time and voltage
         """
 
         file_data = pd.read_csv(self.filename, header= None)
-        file_data_to_numpy = file_data.as_matrix()
-        return file_data_to_numpy
+        self.file_data = file_data.as_matrix()
+        return self.file_data
 
-    def make_object(self, file_data_to_numpy):
+    def make_time_voltage_object(self):
         """Function turns numpy array into object to be manipulated later
         
-        :param file_data_to_numpy: numpy array
-        :returns data: instance of data
+        :param self.file_datay: numpy array
+        :returns self.data: instance of data containing times and voltages
         """
-        data = HeartRateData(file_data_to_numpy)
-        return data
+        self.data = HeartRateData(self.file_data)
+        return self.data 
 
- 
+    def export_data(self,filename,attributes):
+        """Function takes list of final attributes and exports json file
+   
+        :param filename: csv filename
+        :param attributes: avg hr, Volt extremes, time duration, num of beats and beat time array
+        :returns json_name: file name of json file
+        """
+
+        import json
+        json_output = [{"avg_hr:": attributes[0]},
+                       {"Voltage Extremes (V):" : attributes[1]},
+                       {"ECG Time Duration (s):": attributes[2]},
+                       {"Number of Beats:": attributes[3]},
+                       {"Array of Heart Beat Times (s):": attributes[4]}]
+
+        json_name = filename.replace('.csv', '.json')
+        jsonfile = open(json_name, 'w')
+        json.dump(json_output, jsonfile)
+        jsonfile.write('\n')
+
 class HeartRateData:
     
     def __init__ (self, data, index_time= None):
         self.data = data
         self.index_time = index_time
-    
+        self.smooth_voltage = None
+        self.min_max = None
+        self.time_duration = None
+        self.beat_times = None
+        self.bpm = None
+        self.corr_values_class = None
+        self.num_beats = None
+
     def signal_process(self):
         """ Function removes noise with savgol filter and normalized
 
@@ -43,13 +71,11 @@ class HeartRateData:
         :returns smooth_voltage: single array of voltage
         """
 
-     #   import matplotlib.pyplot as plt
         voltage = self.data[:,1]
         norm_voltage = voltage - np.mean(voltage)
-        smooth_voltage = savgol_filter(norm_voltage, 15,3)
-    #    plt.plot(smooth_voltage)
-   #     plt.show()
-        return smooth_voltage
+        self.smooth_voltage = savgol_filter(norm_voltage, 15,3)
+
+        return self.smooth_voltage
 
     def find_voltage_extremes(self):
         """ Function takes array of voltage and finds maximum and minimum
@@ -60,231 +86,214 @@ class HeartRateData:
 
         min_ = np.min(self.data[:,1])
         max_ = np.max(self.data[:,1])
-        min_max = (min_,max_)
-        print('Voltage Extremes (V):', min_max)
-        return min_max
+        self.min_max = (min_,max_)
+        print('Voltage Extremes (V):', self.min_max)
+
+        return self.min_max
 
     def find_duration(self):
         """ Function returns last value of time array-- Duration of the ECG
    
         :param self.data: unfiltered time and voltage array
-        :return time_duration: last value of time array
+        :return self.time_duration: last value of time array
         """
 
         time = self.data[:,0]
-        time_duration = time[len(time)-1]
-        print('Time Duration of ECG strip (s):', time_duration)
-        return time_duration
+        self.time_duration = time[len(time)-1]
+        print('Time Duration of ECG strip (s):', self.time_duration)
 
-    def find_beat_times(self,data, index_time):
+        return self.time_duration
+
+    def find_beat_times(self, data, index_time):
         """ Function takes index of heart beat events and returns the corresponding time values
        
         :param data: unfiltered time and voltage array
         :param index_time: an instance index of heart beat events
-        :returns beat_times: array of times corresponding to heart beat events
+        :returns self.beat_times: array of times corresponding to heart beat events
         """
-        beat_times = []
         time = data[:,0]
-
+        self.beat_times = []
+      
         for i in index_time:
-            beat_times.append(time[i])
+            self.beat_times.append(time[i])
      
-        print('Times of Beat Event (s):', beat_times)
+        print('Times of Beat Event (s):', self.beat_times)
      
-        return(beat_times)
+        return(self.beat_times)
 
+    def find_number_beats(self):
+        """ Function returns number of heart beat events
 
-    def find_avg_hr(self, beat_times):
+        :param self.beat_times: array of beat times
+        :returns self.num_beats: number of total beats
+        """
+
+        self.num_beats = len(self.beat_times)
+        print('Number of detected Beats:', self.num_beats)
+
+        return self.num_beats
+
+    def find_avg_hr(self):
        
         """Function finds median time interval between heart beat events to calculate BPM
-        :param beat_times: array of times of heart beat of events
-        :returns bpm: average heart beat per minute
+
+        :param self.beat_times: array of times of heart beat of events
+        :returns self.bpm: average heart beat per minute
         """
-        beat_diffs = np.diff(beat_times)
+        beat_diffs = np.diff(self.beat_times)
         median_interval = np.median(beat_diffs)
-        bpm = round(60/median_interval)
-        print('Average Heart Rate (BPM):', bpm)
+        self.bpm = round(60/median_interval)
+        print('Average Heart Rate (BPM):', self.bpm)
         
-        return(bpm)
+        return(self.bpm)
 
-    def max_find_correlation(self, smooth_voltage):
-        """Function finds correlation values by correlating subset max QRS wave to rest of signal
+    def max_find_correlation(self):
+        """Function finds correlation values by correlating subset (max QRS wave) to rest of signal
 
-        :param smooth_voltage: filtered voltage array
-        :param subset_max_index: index of maximum voltage value
-        :param subset: a subset of a single QRS wave based on maximum voltage value
-        :returns corr_values_class: an instance of correlation values based on when heart beat occured
+        :param self.smooth_voltage: filtered voltage array
+        :returns self.corr_values_class: an instance of correlation values based on when heart beat occured
         """
 
-#        import matplotlib.pyplot as plt
-        subset_max_index = argrelmax(smooth_voltage, order = len(smooth_voltage))
+        subset_max_index = argrelmax(self.smooth_voltage, order = len(self.smooth_voltage))
         subset_max_index = subset_max_index[0]        
         subset_max_index = subset_max_index[0]
       
         interval = 30
         if subset_max_index < interval:
             interval = subset_max_index -1
-        if (len(smooth_voltage)-subset_max_index < interval):
-            interval = len(smooth_voltage)- subset_max_index-1 
+        if (len(self.smooth_voltage)-subset_max_index < interval):
+            interval = len(self.smooth_voltage)- subset_max_index-1 
         else:
             interval = interval
 
-        subset = smooth_voltage[(subset_max_index-interval):(subset_max_index+interval)]
+        subset = self.smooth_voltage[(subset_max_index-interval):(subset_max_index+interval)]
         subset = subset - np.mean(subset)
         subset = savgol_filter(subset,15,3)
        
-        corr_values = np.correlate(smooth_voltage, subset)
+        corr_values = np.correlate(self.smooth_voltage, subset)
         corr_values = corr_values -np.mean(corr_values)
         
- #       plt.subplot(2,1,1)
-  #      plt.plot(subset)
-   #     plt.subplot(2,1,2)
-    #    plt.plot(corr_values)
-     #   plt.show()
+        self.corr_values_class = DetectHeartBeat(corr_values)
 
-        corr_values_class = DetectHeartBeat(corr_values)
-        return corr_values_class
+        return self.corr_values_class
       
-    
+    def return_attributes(self,duration,min_max):
+        """ Function takes final attributes and exports a list to be used for data exportation
+        
+        :param duration: length of ECG array
+        :param min_max: voltage extremes of ECG array
+        :returns attributes: list of final attributes
+        """
+
+        attributes = [self.bpm, self.num_beats, self.beat_times, duration, min_max]
+      
+        return attributes
+
 
 class DetectHeartBeat:
     def __init__(self,corr_values_class):
         self.corr_values = corr_values_class
+        self.pos_corr_values = None
+        self.beats = None
+        self.num_detected_beats = None
+        self.index_int = None
 
     def get_rid_of_neg(self):
         """Function removes negative correlation values to help detect positive correlation values
 
         :param self.corr_values: instance of correlation values
-        :returns pos_corr_values: only positive correlation values
+        :returns self.pos_corr_values: only positive correlation values
         """
 
-        pos_corr_values = self.corr_values
-        pos_corr_values[pos_corr_values < 0] = 0
+        self.pos_corr_values = self.corr_values
+        self.pos_corr_values[self.pos_corr_values < 0] = 0
         
-        df = pd.DataFrame(pos_corr_values)
+        df = pd.DataFrame(self.pos_corr_values)
         mov_avg = df.rolling(50,center= False).mean()
         
-        poss_corr_values = mov_avg.as_matrix()
-        return pos_corr_values
+        self.poss_corr_values = mov_avg.as_matrix()
 
+        return self.pos_corr_values
 
-    def find_peaks(self, pos_corr_values):
+    def find_peaks(self):
         """Function finds relative max of correlations then filters out values that do not pass threshold.
 
-        :param pos_corr_values: array of positive correlation values
-        :param threshold: threshold to cuttoff what constitutes event, average*5.5
-        :returns beats: array of correlation values at specific heart beat events
+        :param self.pos_corr_values: array of positive correlation values
+        :returns self.beats: array of correlation values at specific heart beat events
         """
 
-      #  import matplotlib.pyplot as plt
-        average = np.mean(pos_corr_values) 
+        average = np.mean(self.pos_corr_values) 
         threshold = average*5.5
+
+       # import matplotlib.pyplot as plt
        # plt.plot(pos_corr_values)
        # plt.plot((0, len(pos_corr_values)),(threshold,threshold), 'k-')
        # plt.show()
 
-        relative_max =argrelmax(pos_corr_values, order = 20)
-        beats = pos_corr_values[relative_max [0]]
-        beats = [item for item in beats if item >= threshold]
+        relative_max =argrelmax(self.pos_corr_values, order = 20)
+        self.beats = self.pos_corr_values[relative_max [0]]
+        self.beats = [item for item in self.beats if item >= threshold]
 
-        return beats
+        return self.beats
 
-    def number_beats(self, beats):
-        """Function takes array of beat correlation values and returns the number of beats
-  
-        :param beats: array of correlation values at heart beat event
-        :returns num_detected_beats: integer value number of beats
-        """
 
-        num_detected_beats = len(beats)
-        print('Number of Detected Beats:', num_detected_beats)
-
-        return num_detected_beats
-
-    def find_peak_index(self, beats, pos_corr_values):
+    def find_peak_index(self):
         """Function that takes relative max correlation values and finds indicies (heart beats)
 
-        :param beats: array of correlation values at heart beat event
-        :param pos_corr_values: array of correlation values
-        :returns index_int: index of maximum correlation values (index of heart beat)
+        :param self.beats: array of correlation values at heart beat event
+        :param self.pos_corr_values: array of correlation values
+        :returns self.index_int: index of maximum correlation values (index of heart beat)
         """
         
-        index_int = []
-        for beat_index, k in enumerate(beats):
-            for index_interval, j in enumerate(pos_corr_values):
+        self.index_int = []
+        for beat_index, k in enumerate(self.beats):
+            for index_interval, j in enumerate(self.pos_corr_values):
            
-                if j == beats[beat_index]:
-                    index_int.append(index_interval)
+                if j == self.beats[beat_index]:
+                    self.index_int.append(index_interval)
 
-        return index_int
+        return self.index_int
 
-    def make_object_time(self, index_int):
-        index_time = HeartRateData(index_int)
-        return index_time
+    def make_object_time(self):
+        """ Function that makes heart beat time instance
+        
+        :param self.index_int: index of maximum correlation values (indicies of heart beats)
+        :returns self.index_time: object containing indicies of heart beats
+        """
 
-def export_data(filename):
-    """ Function takes all data and writes it into json file
- 
-    :param filename: csv filename
-    :param attributes: avg hr, voltage extremes, ECG time duration, Number of beats, Beat times
-    :returns json_name: file name of json file
-    """
+        self.index_time = HeartRateData(self.index_int)
+        
+        return self.index_time
 
-    import json
-    json_name = filename.replace('.csv', '.json')
-    attributes = get_data(filename)
-    data = [{"avg_hr:" : attributes[0]},
-            {"voltage extremes (V):": attributes[1]}, 
-            {"ECG time duration (s):": attributes[2]},
-            {"Number of Beats:": attributes[3]},
-            {"array of heart beat times (s):": attributes[4]}]
-    jsonfile = open(json_name, 'w')
-    json.dump(data, jsonfile)
-    jsonfile.write('\n')
- 
-    return json_name 
 
-def get_data(filename):
-
-    """ Function that messily calls all classes and functions to return data
-    
-    :param filename: csv filename
-    :returns attributes: list of hr, volt_extremes, duration, num_beats, beat_times
-    """
+def main(filename):
 
     #Import Data & create voltage/time object
-    x = ImportData(filename)
-    data = x.read_in_data()
-    data_obj = x.make_object(data)
+    x = ReadWriteData(filename)
+    data =x.read_in_data()
+    data_obj =x.make_time_voltage_object()
 
     #Determine voltage extremes and length of timeframe
-    volt_extremes = data_obj.find_voltage_extremes()
+    min_max = data_obj.find_voltage_extremes()
     duration = data_obj.find_duration()
 
     #Determine Correlation and create object of peak events
-    smooth_voltage =data_obj.signal_process()
-    corr_values_class = data_obj.max_find_correlation(smooth_voltage)   
-    pos_corr_values = corr_values_class.get_rid_of_neg()
-    beats = corr_values_class.find_peaks(pos_corr_values)
-   
-    #Determine Number of beats
-    num_beats = corr_values_class.number_beats(beats)
-    
+    data_obj.signal_process()
+    corr_values_class = data_obj.max_find_correlation()   
+    corr_values_class.get_rid_of_neg()
+    corr_values_class.find_peaks()
+      
     #Determine Time stamp of beats and create object of time values
-    index_int = corr_values_class.find_peak_index(beats,pos_corr_values)
-    index_time = corr_values_class.make_object_time(index_int)   
+    index_int = corr_values_class.find_peak_index()
+    index_time = corr_values_class.make_object_time()   
    
-    #Determine time of beat events and average heart rate
-    beat_times= index_time.find_beat_times(data,index_int)
-    hr = index_time.find_avg_hr(beat_times)
-    
-    attributes = [hr, volt_extremes, duration, num_beats, beat_times]   
+    #Determine time of beat events, number of beats and average heart rate
+    index_time.find_beat_times(data, index_int)
+    index_time.find_avg_hr()
+    index_time.find_number_beats() 
 
-    return attributes
-    
-
-def main(filename):
-#    get_data(filename)
-    export_data(filename)
+    # Organize and export final attributes
+    attributes = index_time.return_attributes_object(duration, min_max) 
+    x.export_data(filename,attributes)
 
 #main('test_data4.csv')
--- test_HRM.py
+++ test_HRM.py
@@ -4,14 +4,14 @@ import numpy as np
 
 
 def test_import():
-    from HRM import ImportData
-    x = ImportData('test_data1.csv')
+    from HRM import ReadWriteData
+    x = ReadWriteData('test_data1.csv')
     voltage = x.read_in_data()
     assert(voltage[0,0] == 0)
     assert(voltage[8,1] == -.12)
 
 from HRM import HeartRateData
-data_matrix = np.array([[1,2],[3,9],[-1212,-1]])
+data_matrix = np.array([[1,2],[3,9],[-1212,-1], [50,4]])
 x = HeartRateData(data_matrix)
 
 def test_min_max_extremes(): 
@@ -21,24 +21,29 @@ def test_min_max_extremes():
     
 def test_duration():
     duration = x.find_duration()
-    assert duration == -1212
-
+    assert duration == 50
 
 def test_index_finder():
-    index_time = x.find_beat_times(data_matrix, [1,2])
-    assert index_time == [3, -1212]    
+    index_time = x.find_beat_times(data_matrix, [1,2,3])
+    assert index_time == [3, -1212,50]    
 
+def test_num_beats():
+    num_beats = x.find_number_beats()
+    assert num_beats == 3
 
 average_matrix= np.array([1,3,6,10])
 y = HeartRateData(average_matrix)
 
 def test_average():
-    average = y.find_avg_hr(average_matrix)
-    assert  average == 20
+    average = x.find_avg_hr()
+    assert  average == 3
 
+def test_attribute_return():
+    attributes = x.return_attributes(25, [1,2])
+    assert len(attributes) ==5 
 
 from HRM import DetectHeartBeat
-data_array = np.array([1,2,3,19191,23,41,-2,41,1,2,3,4,5,6,7,8,8,2,2,4,24,32,1,41,141,13,3,3,3,3])
+data_array = np.array([1,-21,1,1,4,490,5,-1,1,2,3,4,5,-1,1,5,5,2,2,4,-1,32,1,1,1,3,3,3,3,3])
 z = DetectHeartBeat(data_array)
 
 def test_positive_values():
@@ -51,7 +56,7 @@ def test_positive_values():
 
 def test_beat_threshold():
     avg = np.mean(data_array)*5.5
-    values = z.find_peaks(data_array)
+    values = z.find_peaks()
     if (values>= avg).all():
         x = 0
     else:
@@ -59,16 +64,7 @@ def test_beat_threshold():
     assert x == 0
 
 def test_find_index():
-    values = z.find_peak_index([19191], data_array)
-    assert values == [3]
+    values = z.find_peak_index()
+    assert values == [5]
 
-from HRM import export_data
-from HRM import get_data
-def test_output_data():
-    attributes = get_data('test_data1.csv')
-    assert len(attributes) == 5
 
-def test_json_writting():
-    json_file_name = export_data('test_data1.csv')
-    assert (json_file_name == 'test_data1.json')
-     
